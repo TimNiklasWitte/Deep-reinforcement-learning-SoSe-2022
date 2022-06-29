55d7bdbfd0dd7095108065dc4016b21c6ef6a561
TimNiklasWitte <wittet@uos.de>
2022-06-29 20:38:37 +0200

    fix algo

--- Homework 04/PolicyNet.py
+++ Homework 04/PolicyNet.py
@@ -1,3 +1,4 @@
+from tkinter import Y
 import tensorflow as tf
 
 class PolicyNet(tf.keras.Model):
@@ -8,10 +9,15 @@ class PolicyNet(tf.keras.Model):
         self.layer_list = [
             tf.keras.layers.Conv2D(8, kernel_size=(3, 3), strides=(3,3), padding="same", activation="tanh"),
             tf.keras.layers.Conv2D(10, kernel_size=(3, 3), strides=(3,3), padding="same", activation="tanh"),
-            tf.keras.layers.Flatten(),
-            tf.keras.layers.Dense(3, activation='sigmoid')
+            tf.keras.layers.Flatten()
         ]
- 
+
+        self.mu_0 = tf.keras.layers.Dense(1, activation='tanh')
+        self.mu_1_2 = tf.keras.layers.Dense(2, activation='sigmoid')
+
+        self.sigma_0 = tf.keras.layers.Dense(1, activation='sigmoid')
+        self.sigma_1_2 = tf.keras.layers.Dense(2, activation='sigmoid')
+
         self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
    
     @tf.function
@@ -20,7 +26,32 @@ class PolicyNet(tf.keras.Model):
         for layer in self.layer_list:
             x = layer(x)
 
-        return x
+        mu_0 = self.mu_0(x)
+        mu_1_2 = self.mu_1_2(x)
+ 
+        sigma_0 = self.sigma_0(x)
+        sigma_1_2 = self.sigma_1_2(x)
+
+        # reparameterization trick
+        batch_size = x.shape[0]
+        epsilon_0 = tf.random.normal(shape=(batch_size, 1))
+        y_0 = mu_0 + epsilon_0 * sigma_0
+        
+        epsilon_2_3 = tf.random.normal(shape=(batch_size, 1))
+        y_2_3 = mu_1_2 + epsilon_2_3 * sigma_1_2
+
+        # clipping
+        y_0 = tf.clip_by_value(y_0, -1, 1)
+        y_2_3 = tf.clip_by_value(y_2_3, 0, 1)
+
+        actions = tf.concat([y_0, y_2_3], axis=-1)
+
+        # scale y_0 from [-1, 1] to [0, 1]
+        y_0 = (y_0 + 1 + 0.000001) / 2 # avoid div by 0
+        probs_actions = tf.concat([y_0, y_2_3], axis=-1)
+        log_probs_actions = tf.math.log(probs_actions)
+
+        return actions, log_probs_actions
             
 
     @tf.function
@@ -28,9 +59,8 @@ class PolicyNet(tf.keras.Model):
         
 
         with tf.GradientTape() as tape:
-            prob_actions = self(state)
-            log_prob_actions = tf.math.log(prob_actions)
-            update = -tf.math.multiply(g_t[:, tf.newaxis], log_prob_actions)
+            _, log_probs_actions = self(state)
+            update = -tf.math.multiply(g_t[:, tf.newaxis], log_probs_actions)
         
         gradients = tape.gradient(update, self.trainable_variables)
  
--- Homework 04/Training.py
+++ Homework 04/Training.py
@@ -15,63 +15,53 @@ def preprocess(state):
     state = (state / 128.) - 1
     return state
 
+
+def make_env():
+    return gym.make("CarRacing-v1")
+
 def main():
 
     # Logging
     file_path = "test_logs/test"
     train_summary_writer = tf.summary.create_file_writer(file_path)
 
-    num_envs = 15
-    episode_len = 1000
+    batch_size = 16
+    episode_len = 500
     gamma = 0.99
-    batch_size = 32
+        
+    env = gym.vector.make("CarRacing-v1", num_envs=batch_size)
 
-    
-    env = gym.vector.make("CarRacing-v0", num_envs=num_envs)
-    env.close()
     policy_net = PolicyNet()
     policy_net.build(env.observation_space.shape)
     policy_net.summary()
 
-    for num_episode in range(250):
-        
-        env = gym.vector.make("CarRacing-v0", num_envs=num_envs)
 
-        #
-        # Sampling
-        #
- 
-        buff_states = np.zeros(shape=(episode_len, num_envs, *env.observation_space.shape[1:]), dtype=np.uint8)
-        buff_rewards = np.zeros(shape=(episode_len, num_envs), dtype=np.float32)
+    for num_episode in range(250):
+
+        buff_states = np.zeros(shape=(episode_len, batch_size, *env.observation_space.shape[1:]), dtype=np.float32)
+        buff_rewards = np.zeros(shape=(episode_len, batch_size), dtype=np.float32)
 
         states = env.reset()
         for idx in tqdm.tqdm(range(episode_len), position=0, leave=True):
-
             states = preprocess(states)
-            action = policy_net(states)
-
-            action = action.numpy() # remove batch dim and convert to numpy
-            action[:,0] = 2*action[:,0] - 1
-            
-            next_states, rewards, dones , _ = env.step(action)
+            actions, _ = policy_net(states)
+            actions = actions.numpy()
+         
+            next_states, rewards, dones , _ = env.step(actions)
 
             buff_states[idx] = states
             buff_rewards[idx] = rewards
 
             if np.any(dones):
-                states = env.reset()
+                next_states = env.reset()
 
             states = next_states
         
-        env.close()
         # Evaluation: Consider only first env
-
         rewards = buff_rewards[:, 0]
-
         score = np.sum(rewards)
         avg_rewards = np.mean(rewards)
         
-
         print(f"  Episode: {num_episode}")
         print(f"    Score: {round(score,2)}")
         print(f"Avg Score: {round(avg_rewards, 2)}")
@@ -82,39 +72,16 @@ def main():
             tf.summary.scalar(f"Score", score, step=num_episode)
 
 
-        # calc return
-        buff_returns = np.zeros(shape=(episode_len, num_envs), dtype=np.float32)
-
         for start_idx in range(episode_len):
 
-            g_t = np.zeros(shape=(num_envs,), dtype=np.float32)
+            g_t = np.zeros(shape=(batch_size,), dtype=np.float32)
             for i in range(episode_len - start_idx): #range(len(rewards[j:])):
                 rewards = buff_rewards[start_idx + i, :]
                 g_t += (gamma**i) * rewards
 
-            buff_returns[start_idx] = g_t
+            states = buff_states[start_idx]
 
-        # Merge two axes: episode_len and num_envs 
-        buff_size =  episode_len * num_envs 
-        buff_states = np.reshape(buff_states, newshape=(buff_size, *env.observation_space.shape[1:]) )
-        buff_returns = np.reshape(buff_returns, newshape=(buff_size,))
-
-
-        #
-        # Training
-        #
-
-        # sample batch
-        rng = default_rng()
-        batch_indices = rng.choice(buff_size, size=batch_size, replace=False)
-
-        sampled_states = buff_states[batch_indices, ...]
-        sampled_states = preprocess(sampled_states)
-
-        sampled_returns = buff_returns[batch_indices]
-
-        policy_net.train_step(sampled_states, sampled_returns)
-    
+            policy_net.train_step(states, g_t)
    
 
 
