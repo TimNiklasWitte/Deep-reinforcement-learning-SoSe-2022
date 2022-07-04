f4ca12e7c8f225205a807cfda4c1a2c2cbef8514
TimNiklasWitte <wittet@uos.de>
2022-07-04 18:32:37 +0200

    fix algo

--- Homework 04/A2C/Training.py
+++ Homework 04/A2C/Training.py
@@ -20,7 +20,6 @@ def main():
     train_summary_writer = tf.summary.create_file_writer(file_path)
 
     batch_size = 32
-    episode_len = 50
     gamma = 0.99
         
     env = gym.vector.make("CarRacing-v1", num_envs=batch_size)
@@ -35,7 +34,7 @@ def main():
 
   
     states = env.reset()
-    for i in range(5000):
+    for i in range(100000):
         
         states = preprocess(states)
         actions, _ = policy_net(states)
@@ -86,61 +85,9 @@ def main():
             tf.summary.scalar(f"Average reward", avg_reward, step=i)
             tf.summary.scalar(f"Loss", loss, step=i)
 
-    return 
-
-    for num_episode in range(1000):
-
-        buff_states = np.zeros(shape=(episode_len, batch_size, *env.observation_space.shape[1:]), dtype=np.float32)
-        buff_rewards = np.zeros(shape=(episode_len, batch_size), dtype=np.float32)
-
-        states = env.reset()
-        for idx in tqdm.tqdm(range(episode_len), position=0, leave=True):
-            states = preprocess(states)
-            actions, _ = policy_net(states)
-            actions = actions.numpy()
-         
-            next_states, rewards, dones , _ = env.step(actions)
-
-            buff_states[idx] = states
-            buff_rewards[idx] = rewards
-
-            if np.any(dones):
-                next_states = env.reset()
-
-            states = next_states
-        
-        # Evaluation: Consider only first env
-        rewards = buff_rewards[:, 0]
-        score = np.sum(rewards)
-        avg_rewards = np.mean(rewards)
-        
-        print(f"  Episode: {num_episode}")
-        print(f"    Score: {round(score,2)}")
-        print(f"Avg Score: {round(avg_rewards, 2)}")
-        print("------------------------") 
-
-        with train_summary_writer.as_default():
-            tf.summary.scalar(f"Average reward", avg_rewards, step=num_episode)
-            tf.summary.scalar(f"Score", score, step=num_episode)
-
-
-
-        buff_returns = np.zeros(shape=(episode_len, batch_size), dtype=np.float32)
-        for start_idx in range(episode_len):
-
-            g_t = np.zeros(shape=(batch_size,), dtype=np.float32)
-            for i in range(episode_len - start_idx): #range(len(rewards[j:])):
-                rewards = buff_rewards[start_idx + i, :]
-                g_t += (gamma**i) * rewards
-
-            buff_returns[start_idx] = g_t
-
-
-        policy_net.train_step(buff_states, buff_returns)
-
-        if num_episode % 10 == 0:
-            policy_net.save_weights(f"./saved_models/trained_weights_episode_{num_episode}", save_format="tf")
-
+        if i % 1000:
+            policy_net.save_weights(f"./saved_models/policy_net/trained_weights_{i}", save_format="tf")
+            value_net.save_weights(f"./saved_models/value_net/trained_weights_{i}", save_format="tf")
    
 
 
