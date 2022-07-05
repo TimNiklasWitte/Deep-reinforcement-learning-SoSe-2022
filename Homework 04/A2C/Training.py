7171b0519e176c4b718d83564f4610643708399f
TimNiklasWitte <wittet@uos.de>
2022-07-05 15:21:53 +0200

    add a2c

--- Homework 04/A2C/Training.py
+++ Homework 04/A2C/Training.py
@@ -4,8 +4,6 @@ import numpy as np
 from PolicyNet import *
 from ValueNet import *
 
-import tqdm
-
 def preprocess(state):
 
     state = np.cast['float32'](state)
@@ -15,40 +13,50 @@ def preprocess(state):
 
 def main():
 
-    train_steps = 10000
-    steps_until_reset = 500
-    steps_until_evalution = 100
-    evaluation_steps = 100
-    steps_until_modelSave = 1000
-
     # Logging
     file_path = "test_logs/test"
     train_summary_writer = tf.summary.create_file_writer(file_path)
 
-    batch_size = 32
     gamma = 0.99
+    num_episodes = 1000
+    episode_len = 50
+    episodes_until_saveNet = 25
 
-    env = gym.vector.make("CarRacing-v1", num_envs=batch_size)
+    env = gym.make("CarRacing-v1")
 
     policy_net = PolicyNet()
-    policy_net.build(env.observation_space.shape)
+    policy_net.build((1, *env.observation_space.shape))
     policy_net.summary()
 
     value_net = ValueNet()
-    value_net.build(env.observation_space.shape)
+    value_net.build((1, *env.observation_space.shape))
     value_net.summary()
 
-  
-    states = env.reset()
-    
-    for step_num in range(train_steps):
-        
-        states = preprocess(states)
-        actions, _ = policy_net(states)
 
-        actions = actions.numpy()
-        
-        next_states, rewards, dones , _ = env.step(actions)
+    for num_episode in range(num_episodes):
+
+        states = np.zeros(shape=(episode_len, *env.observation_space.shape))
+        next_states = np.zeros(shape=(episode_len, *env.observation_space.shape))
+        rewards = np.zeros(shape=(episode_len,))
+
+        state = env.reset()
+        for step_num in range(episode_len):
+            state = preprocess(state)
+            state = np.expand_dims(state, axis=0)
+            action, _ = policy_net(state)
+            action = action.numpy()
+            action = action[0] # remove batch dim
+
+            next_state, reward, done , _ = env.step(action)
+
+            states[step_num] = state
+            next_states[step_num] = next_state
+            rewards[step_num] = reward
+
+            state = next_state
+            if done:
+                state = env.reset()
+    
 
         #
         # Train policy net
@@ -60,63 +68,55 @@ def main():
         next_states = preprocess(next_states)
         rewards = np.expand_dims(rewards, axis=-1)
         advantages = rewards + gamma*value_net(next_states) - value_net(states)
-
-        policy_net.train_step(states, advantages)
+        policy_net.train_step(states, advantages) 
 
         # 
         # Train value net
         #
         values_target = rewards + gamma*value_net(next_states)
         loss = value_net.train_step(states, values_target)
-     
 
-        states = next_states
-        if np.any(dones) or step_num % steps_until_reset == 0:
-            states = env.reset()
+
+        # Evalation
+        env_test = gym.make("CarRacing-v1")
+        state = env_test.reset()
+
+        rewards = []
+        for test_run_step in range(episode_len):
+            state = preprocess(state)
+            state = np.expand_dims(state, axis=0)
+            action, _ = policy_net(state)
+            action = action.numpy()
+            action = action[0]
+                
+            next_state, reward, done , _ = env_test.step(action)
+
+            state = next_state
+            rewards.append(reward)
+            if done:
+                break 
+            
+        env_test.close()
+        avg_reward = np.mean(rewards)
+
+            
+        print(f" Iteration: {num_episode}")
+        print(f"Avg reward: {avg_reward}")
+        print(f"      Loss: {loss}")
+        print("------------------------")
 
 
         #
         # Log
         #
+        loss = loss.numpy()
+        with train_summary_writer.as_default():
+            tf.summary.scalar(f"Average reward", avg_reward, step=num_episode)
+            tf.summary.scalar(f"Loss", loss, step=num_episode)
 
-        # Evalation
-        if step_num % steps_until_evalution == 0:
-            env_test = gym.make("CarRacing-v1")
-            state = env_test.reset()
-
-            rewards = []
-            for test_run_step in range(evaluation_steps):
-                state = preprocess(state)
-                state = np.expand_dims(state, axis=0)
-                action, _ = policy_net(state)
-                action = action.numpy()
-                action = action[0]
-                
-                next_state, reward, done , _ = env_test.step(action)
-
-                state = next_state
-                rewards.append(reward)
-                if done:
-                    break 
-            
-            env_test.close()
-            avg_reward = np.mean(rewards)
-
-            
-            print(f" Iteration: {step_num}")
-            print(f"Avg reward: {avg_reward}")
-            print(f"      Loss: {loss}")
-            print("------------------------")
-
-            loss = loss.numpy()
-            with train_summary_writer.as_default():
-                tf.summary.scalar(f"Average reward", avg_reward, step=step_num)
-                tf.summary.scalar(f"Loss", loss, step=step_num)
-            
-
-        if step_num % steps_until_modelSave == 0:
-            policy_net.save_weights(f"./saved_models/policy_net/trained_weights_{step_num}", save_format="tf")
-            value_net.save_weights(f"./saved_models/value_net/trained_weights_{step_num}", save_format="tf")
+        if num_episode % episodes_until_saveNet == 0:
+            policy_net.save_weights(f"./saved_models/policy_net/trained_weights_{num_episode}", save_format="tf")
+            value_net.save_weights(f"./saved_models/value_net/trained_weights_{num_episode}", save_format="tf")
    
 
 
